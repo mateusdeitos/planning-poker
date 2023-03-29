a42f2afb0a20f5395945d8dccba338a142c0184a
Mateus Deitos <mate.deitos@hotmail.com>
2023-03-29 10:15:00 -0300

    refactor: sempre exibir o nome do membro quando for mobile

--- src/components/Voting/MemberCard.tsx
+++ src/components/Voting/MemberCard.tsx
@@ -1,4 +1,4 @@
-import { Avatar, CardHeader, Tooltip } from "@chakra-ui/react";
+import { Avatar, CardHeader, Tooltip, useBreakpointValue } from "@chakra-ui/react";
 import { useRoomDetails } from "../../hooks/useRoomDetails";
 import { useRoomIdFromRouter } from "../../hooks/useRoomIdFromRouter";
 import { App } from "../../types";
@@ -8,20 +8,30 @@ export const MemberCard = ({ member }: { member: App.User; }) => {
 	const roomId = useRoomIdFromRouter();
 	const { data, isSuccess } = useRoomDetails(roomId);
 	const label = data?.cards.find(card => card.value === member.vote)?.label ?? "?";
+	const isMobile = useBreakpointValue({ base: true, md: false });
 
 	if (!isSuccess) return null;
 
 	return <VotingCard>
 		<CardHeader pos="absolute" top="-43px" left="14px">
-			<Tooltip label={member.displayName} hasArrow>
-				<Avatar
-					size="sm"
-					name={member.displayName}
-					src={member.photoURL} />
-			</Tooltip>
+			{isMobile ? (
+				<Tooltip label={member.displayName} isOpen placement="bottom">
+					<Avatar
+						size="sm"
+						name={member.displayName}
+						src={member.photoURL} />
+				</Tooltip>
+			) : (
+				<Tooltip label={member.displayName} hasArrow placement="top">
+					<Avatar
+						size="sm"
+						name={member.displayName}
+						src={member.photoURL} />
+				</Tooltip>
+			)}
 		</CardHeader>
 		{data?.votingState !== "finished" && member.voteStatus === "not-voted" && <VotingCard.NotVotedBody />}
 		{data?.votingState !== "finished" && member.voteStatus === "voted" && <VotingCard.VotedBody />}
 		{data?.votingState === "finished" && <VotingCard.RevealedBody value={label} />}
-	</VotingCard>;
+	</VotingCard >;
 };
